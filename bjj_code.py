ddf403231a8d4d15f819b86a3d18112fca485f5a
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2019-09-23 22:20:27 +0200

    Update bjj_code.py
    - new questions analysed (demography)
    - pie charts
    - bar plots
    - stacked area plots

--- bjj_code.py
+++ bjj_code.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from collections import Counter, OrderedDict
 
@@ -22,7 +23,7 @@ data_q = data[qestions_order][2:]
 
 ######################## cleaning columns   ##########################
 
-freetext_variables = [18,19,20,26,28,39,40,41,43,50,61,63,65,66,67,68]
+# freetext_variables = [18,19,20,26,28,39,40,41,43,50,61,63,65,66,67,68]
 
 ################## 1. Country Q67 --> "nationality"  #################
 
@@ -162,6 +163,8 @@ def bar_plot(question):
     
 bar_plot('Q56')
 
+bar_plot('Q2')
+
 ################## Q57 - age ##################
 #print(colnames_dict['Q57'])
 age_data = data['Q57'][data['Q57'] != '']
@@ -203,3 +206,71 @@ pie_chart_generator('Q59')
 # belt promotion time Kaplan Meier
 # do BJJ people buy naitonal brands?
 # do higher belts compete more often?
+
+
+data[['Q55','Q2']].groupby(['Q55','Q2']).size()
+
+#############  pie chart visualising the answers  #####################
+
+belt_colours = ['White belt','Blue Belt','Purple Belt','Brown Belt','Black Belt']
+
+def pie_chart_by(question, gender = False, belt = False):
+   
+    if gender != False: 
+        for gender_ in ('Female','Male'):
+            question_list = data[question][(data[question] != '') & (data['Q55'] == gender_)]
+            
+            n = sum([1 for x in data['Q55'] if x == gender])
+            
+            counts = Counter(question_list[2:].tolist())
+            plt.pie([int(v) for v in counts.values()], 
+                     labels=[str(k) for k in counts.keys()],
+                     autopct='%.1f%%')
+            plt.title(gender_ + " (" + str(n) + ")")
+            plt.show()
+     
+    if belt != False: 
+        for belt_ in list(belt_colours.values()):
+            question_list = data[question][(data[question] != '') & (data['Q2'] == belt_)]
+            
+            n = sum([1 for x in data['Q2'] if x == belt_])
+            
+            counts = Counter(question_list[2:].tolist())
+            plt.pie([int(v) for v in counts.values()], 
+                     labels=[str(k) for k in counts.keys()],
+                     autopct='%.1f%%')
+            plt.title(belt_ + " (" + str(n) + ")")
+            plt.show()   
+            
+            
+pie_chart_by('Q2', gender = True)
+
+pie_chart_by('Q67.1', gender = True)   
+
+pie_chart_by('Q67.1', belt = True)
+
+
+cross = pd.crosstab(data['Q2'][data['Q2'].isin(belt_colours)], data['Q67.1'][data['Q67.1'] != ''])
+cross_perc = cross.divide(cross.sum(axis=1), axis = 0)
+cross_perc2 = pd.DataFrame(cross_perc, index = belt_colours)
+
+cross2 = pd.DataFrame(cross, index = belt_colours)
+techniques = ['Closed guard', 'Leg locking', 'Open Guard', 'Pressure Passing']
+# Do preferences change over time?
+
+# Make the plot
+plt.stackplot(belt_colours, cross_perc['Closed guard'],cross_perc['Leg locking'],cross_perc['Open Guard'],cross_perc['Pressure Passing'], labels=techniques)
+plt.legend(loc='upper left')
+#plt.xticks(belt_colours)
+plt.title('Favourite techniques vs. belt colour')
+plt.show()
+
+# Make the plot
+plt.stackplot(belt_colours, cross_perc2['Closed guard'],cross_perc2['Leg locking'],cross_perc2['Open Guard'],cross_perc2['Pressure Passing'], labels=techniques)
+plt.legend(loc='upper left')
+#plt.xticks(belt_colours)
+plt.title('Favourite techniques vs. belt colour')
+plt.show()
+
+
+
